aaf18c3696bc0017412ebad744277751bb085ac3
Parsa Ghorbani <parsa_ghorbani@outlook.com>
2019-07-16 20:00:12 +0430

    User Manager for validation

--- myapp/models.py
+++ myapp/models.py
@@ -1,12 +1,25 @@
 from django.db import models
+from django.db.models.functions import Length
 from django.core.validators import MaxValueValidator, MinValueValidator, MinLengthValidator
+
+class ValidDataManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().annotate(len_fname=Length('first_name'), len_lname=Length('last_name')).filter(age__gte=18,
+                                                                                                            age__lte=99,
+                                                                                                            experience__gte=0,
+                                                                                                            experience__lte=240,
+                                                                                                            len_fname__gte=2,
+                                                                                                            len_fname__lte=32,
+                                                                                                            len_lname__gte=4,
+                                                                                                            len_lname__lte=64)
 
 class MyModel(models.Model):
     # age = models.PositiveIntegerField(validators=[MinValueValidator(18), MaxValueValidator(99)], null=False)
     # experience = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(240)], null=False)
     # first_name = models.CharField(validators=[MinLengthValidator(2)], max_length=32, null=False)
     # last_name = models.CharField(validators=[MinLengthValidator(4)], max_length=64, null=False)
-
+    object = models.Manager()
+    valid_manager = ValidDataManager()
     age = models.PositiveIntegerField(null=True)
     experience = models.PositiveIntegerField(null=True)
     first_name = models.CharField(null=True, max_length=100)
--- phase4.py
+++ phase4.py
@@ -1,7 +1,7 @@
 import django.db
 from django.db.models.functions import Length
 from myapp.models import MyModel
-
+from django.db import models
 
 invalid_ages = (MyModel.objects.filter(age__gt=99) | MyModel.objects.filter(age__lt=18)).distinct()
 invalid_experience = (MyModel.objects.filter(experience__gt=24) |MyModel.objects.filter(experience__lt=0)).distinct()
@@ -10,14 +10,8 @@ invalid_last_name_len = (MyModel.objects.annotate(last_name_length=Length('last_
 invalid_data = (invalid_ages | invalid_experience | invalid_first_name_len | invalid_last_name_len).distinct()
 
 
-valid_data = MyModel.objects.annotate(len_fname=Length('first_name'), len_lname=Length('last_name')).filter(age__gte=18,
-                                                                                                            age__lte=99,
-                                                                                                            experience__gte=0,
-                                                                                                            experience__lte=240,
-                                                                                                            len_fname__gte=2,
-                                                                                                            len_fname__lte=32,
-                                                                                                            len_lname__gte=4,
-                                                                                                            len_lname__lte=64)
+valid_manager = ValidDataManager()
+valid_data = MyModel.valid_manager.all()
 
 mahdi_21 = (valid_data.filter(first_name="mahdi") | valid_data.filter(age__lte=21)).distinct()
 ali_31_24 = valid_data.filter(first_name__contains="ali", age__lt=30, experience__gt=24)
